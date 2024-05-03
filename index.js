88be5d1b3eb5540a619bbd832d31706220e26718
atikur28 <atikur45683@gmail.com>
2024-05-03 22:02:27 +0600

    Admin and Manager routing Hooks and Routes added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -29,7 +30,64 @@ async function run() {
 
     const usersCollection = client.db("resultProcessingSystem").collection("users");
 
+    // jwt
+    app.post("/jwt", async (req, res) => {
+      const users = req.body;
+      const token = jwt.sign(users, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      res.send({ token });
+    });
 
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access!" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: "forbidden access!" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+  
+    
+    // users
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user) {
+        isAdmin = user?.role === "admin";
+      }
+      res.send({ isAdmin });
+    });
+
+    app.get("/users/manager/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isManager = false;
+      if (user) {
+        isManager = user?.role === "manager";
+      }
+      res.send({ isManager });
+    });
+    
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
